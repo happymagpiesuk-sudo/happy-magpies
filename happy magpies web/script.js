53610e92c51b59e199afb9f2be9a24332728c21d
happymagpiesuk-sudo <happymagpiesuk@gmail.com>
2026-07-19 20:13:04 +0100

    Delete happy magpies web directory

--- happy magpies web/script.js
+++
@@ -1,9 +0,0 @@
-function showSignupMessage() {
-    const message = document.getElementById("signup-message");
-
-    message.classList.add("show");
-
-    setTimeout(() => {
-        message.classList.remove("show");
-    }, 5000);
-}
